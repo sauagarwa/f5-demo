7bec6a6c5441d09ec0150ae35f3043d94b9c7023
sauagarwa <sauagarw@redhat.com>
2025-06-04 10:23:16 -0400

    Added llama stack and ui service

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,13 +5,22 @@ echo -e "\nYour Hugging Face token is $HF_TOKEN"
 
 NAMESPACE=f5-demo
 
+echo "Creating namespace $NAMESPACE"
 oc new-project $NAMESPACE
+
+echo "Installing LLMs in namespace $NAMESPACE. This will take around 15-20 mins"
 helm upgrade --install llm-service llm-service \
---set models.llama-3-2-1b-instruct.enabled=true \
+--set models.llama-3-2-3b-instruct.enabled=true \
 --set models.llama-3-2-1b-instruct-quantized.enabled=true \
 --set secret.hf_token="$HF_TOKEN" -n $NAMESPACE
 
+echo "Installing LLama Stack server in namespace $NAMESPACE"
+helm upgrade --install llama-stack llama-stack \
+--set models.llama-3-2-3b-instruct.enabled=true \
+--set models.llama-3-2-1b-instruct-quantized.enabled=true
 
+echo "Installing F5 Demo UI server in namespace $NAMESPACE"
+helm upgrade --install demo-ui demo-ui
 
 
 
--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -2,6 +2,19 @@
 
 NAMESPACE=f5-demo
 
-helm delete llm-service llm-service -n $NAMESPACE
+echo "Uninstalling F5 Demo..."
+
+echo "Removing Demo UI..."
+helm delete demo-ui -n $NAMESPACE
+
+echo "Removing LLama Stack server..."
+helm delete llama-stack -n $NAMESPACE
+
+echo "Removing LLMs..."
+helm delete llm-service  -n $NAMESPACE
+
 sleep 30
+
+echo "Deleting namespace f5-demo..."
 oc delete namespace $NAMESPACE
+
